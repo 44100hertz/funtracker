29228510b6b4d22046a047badda75a1f4b182f22
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-21 18:27:54 -0600

    simplified initialization

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,18 @@ mod channel;
 
 fn main() {
     let mut sng = song::Song {
-        channels: Vec::new(),
+        channels: vec![
+            channel::Channel {
+                samp_off: 0,
+                samp_len: 20,
+                samp_rate: 0f32,
+                wave: 0f32,
+                phase: 0f32,
+                volume: 1f32,
+                note: 0,
+            }
+        ]
     };
-
-    sng.channels.push(
-        channel::Channel {
-            samp_off: 0,
-            samp_len: 20,
-            samp_rate: 0f32,
-            wave: 0f32,
-            phase: 0f32,
-            volume: 1f32,
-            note: 0,
-        }
-    );
 
     for _ in 1..100 {
         println!("{}", sng.update());
