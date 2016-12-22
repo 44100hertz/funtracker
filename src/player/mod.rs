f8487a56feadd66b45a789603844232bb0a1bbca
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-22 10:19:23 -0600

    test for note parsing

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,2 +1,19 @@
 pub mod channel;
 pub mod song;
+pub mod base32;
+pub mod parser;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn middle_c() {
+        let note = super::parser::parse_note("C-4");
+        assert_eq!(note, 48)
+    }
+
+    #[test]
+    fn a_sharp_0() {
+        let note = super::parser::parse_note("A#0");
+        assert_eq!(note, 10)
+    }
+}
