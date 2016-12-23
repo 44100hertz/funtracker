c9ac2e9af77ba7702b825e878b2d2fd7e0043286
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-23 15:04:53 -0600

    made note fn's take in double

--- src/player/note.rs
+++ src/player/note.rs
@@ -1,7 +1,7 @@
-pub fn get_freq(num: i32) -> f64 {
-    440.0 * 2f64.powf(((num - 60) as f64 / 12.0))
+pub fn get_freq(num: f64) -> f64 {
+    440.0 * 2f64.powf(((num - 60.0) / 12.0))
 }
 
-pub fn get_period(num: i32) -> f64 {
-    1.0/440.0 * 2f64.powf(-((num - 60) as f64 / 12.0))
+pub fn get_period(num: f64) -> f64 {
+    1.0/440.0 * 2f64.powf(-((num - 60.0) / 12.0))
 }
