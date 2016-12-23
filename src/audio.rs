1eb3a3b8d8a8d341cf30a9d7bcf23bc4eebece48
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-22 23:24:10 -0600

    moved SDL audio stuff into audio

---
+++ src/audio.rs
@@ -0,0 +1,50 @@
+extern crate sdl2;
+use std::time::Duration;
+use std::thread;
+use self::sdl2::audio::{AudioSpecDesired, AudioCallback};
+use self::sdl2::{Sdl, AudioSubsystem};
+
+use player::song::Song;
+
+pub struct Audio {
+    subsystem: AudioSubsystem,
+    srate: i32,
+}
+
+impl Audio {
+    pub fn new(context: Sdl, srate: i32) -> Audio {
+        Audio {
+            subsystem: context.audio()
+                .expect("failed to init sdl2 audio"),
+            srate: srate,
+        }
+    }
+
+    pub fn play_song(&mut self, song: Song) {
+        let desired_spec = AudioSpecDesired {
+            freq: Some(self.srate),
+            channels: Some(1),
+            samples: None,
+        };
+
+        struct ACallback {
+            song: Song,
+        }
+
+        impl AudioCallback for ACallback {
+            type Channel = f32;
+            fn callback(&mut self, out: &mut [f32]) {
+                for point in out.iter_mut() {
+                    *point = self.song.get_point();
+                }
+            }
+        }
+
+        let device = self.subsystem.open_playback(
+            None, &desired_spec, |_| ACallback {song: song})
+            .expect("could not open playback");
+
+        device.resume();
+        thread::sleep(Duration::from_millis(2000));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 extern crate sdl2;
-use self::sdl2::audio::{AudioSpecDesired};
-use std::time::Duration;
 
 mod player;
 use player::song::Song;
 use player::channel::Channel;
 
-mod output;
-use output::Output;
+mod audio;
+use audio::Audio;
 
 fn main() {
     let sng = Song {
@@ -24,25 +22,9 @@ fn main() {
         ]
     };
 
-    let out = Output {
-        song: sng,
-    };
-
     let sdl_context = sdl2::init()
         .expect("failed to init sdl2");
-    let audio_subsystem = sdl_context.audio()
-        .expect("failed to init sdl2 audio");
 
-    let desired_spec = AudioSpecDesired {
-        freq: Some(48000),
-        channels: Some(1),
-        samples: None,
-    };
-
-    let device = audio_subsystem.open_playback(
-        None, &desired_spec, |_| {out})
-        .expect("could not open playback");
-    device.resume();
-
-    std::thread::sleep(Duration::from_millis(2000));
+    let mut out = Audio::new(sdl_context, 48000);
+    out.play_song(sng);
 }
