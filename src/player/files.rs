46c84bb47e87c9239fdeb49d81055a4a538e19bd
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-26 08:17:11 -0600

    Revert "preliminary memory mapping"
    This reverts commit 37d730121a5ecd1dace69a56f045616f1d325240.

--- src/player/files.rs
+++ src/player/files.rs
@@ -1,18 +1,31 @@
+use std::fs::File;
 use std::path::PathBuf;
-use std::io::{Read, Write};
+use std::io::Read;
 use player::song::Song;
 use player::parse;
 
-extern crate memmap;
-use self::memmap::{Mmap, Protection};
-
-pub fn track_from_dir(pathstr: &str) {
+pub fn read_track_from_dir(pathstr: &str) -> Song {
     let mut path = PathBuf::from(pathstr);
-
     path.push("sequence");
-    let patt = Mmap::open_path(&path, Protection::Read).unwrap();
+
+    let sequence = match File::open(&path) {
+        Ok(mut file) => {
+            let mut s = String::new();
+            file.read_to_string(&mut s)
+                .expect("error reading file");
+            parse::parse_seq(&s)
+        },
+        Err(_) => Vec::new(),
+    };
 
     path.pop();
     path.push("samples.raw");
-    let samples = Mmap::open_path(&path, Protection::Read).unwrap();
+
+    let mut samples = Vec::new();
+    if let Ok(mut file) = File::open(&path) {
+        file.read_to_end(&mut samples)
+            .expect("error reading file");
+    }
+
+    Song::new(sequence, samples)
 }
