743b030eac5601dab0f9404b0aad83bd805b0232
Sam Pagenkopf <ssaammp@gmail.com>
2016-12-26 20:31:46 -0600

    robuster command parsing

--- src/player/command.rs
+++ src/player/command.rs
@@ -3,15 +3,17 @@ use player::song::Channel;
 
 /// Set a parameter
 pub fn set(chan: &mut Channel, command: &str) {
+    if command.len()==0 {return};
     let (id, v) = command.split_at(1);
-    match id.as_bytes()[0] as char {
-        '2' => chan.samp_off  = d_num(v, 0.0),
-        '3' => chan.samp_len  = d_num(v, 0.0),
-        '6' => chan.samp_rate = d_num(v, 16000.0),
-        '8' => chan.wave      = d_num(v, 0.0),
-        '9' => chan.phase     = d_num(v, 0.0),
-        'A' => chan.volume    = d_num(v, 0.5),
-        'N' => chan.note      = d_num(v, 48.0),
+    println!("{}", command);
+    match id {
+        "2" => chan.samp_off  = d_num(v, 0.0),
+        "3" => chan.samp_len  = d_num(v, 0.0),
+        "6" => chan.samp_rate = d_num(v, 16000.0),
+        "8" => chan.wave      = d_num(v, 0.0),
+        "9" => chan.phase     = d_num(v, 0.0),
+        "A" => chan.volume    = d_num(v, 0.5),
+        "N" => chan.note      = d_num(v, 48.0),
         _ => {}
     }
 }
